67b06e973f164d364a836e871cbbd85554829f25
atuining <nagpalpranay@gmail.com>
2025-02-02 03:23:23 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::{env, process};
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // display list of args in list to stderr
+    // dbg!(args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem passing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
